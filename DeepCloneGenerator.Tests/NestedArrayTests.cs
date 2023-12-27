762911ce417c8d35685798632f970f0587e8fb82
Sander van 't Einde <github@sandervanteinde.nl>
2023-12-27 23:09:56 +0100

    Support nested arrays, fixes #4 (#7)

---
+++ DeepCloneGenerator.Tests/NestedArrayTests.cs
@@ -0,0 +1,47 @@
+﻿namespace DeepCloneGenerator.Tests;
+
+public partial class NestedArrayTests
+{
+    [Fact]
+    public void Test()
+    {
+        var rand = Random.Shared;
+        var original = new NestedArrayClass
+        {
+            NestedArray = Enumerable.Range(start: 0, rand.Next(maxValue: 3) + 1)
+                .Select(
+                    _ => Enumerable.Range(start: 0, rand.Next(maxValue: 3) + 1)
+                        .Select(_ => rand.Next())
+                        .ToArray()
+                )
+                .ToArray(),
+            DeeplyNestedArray = Enumerable.Range(start: 0, rand.Next(maxValue: 3) + 1)
+                .Select(
+                    _ => Enumerable.Range(start: 0, rand.Next(maxValue: 3) + 1)
+                        .Select(
+                            _ => Enumerable.Range(start: 0, rand.Next(maxValue: 3) + 1)
+                                .Select(
+                                    _ => Enumerable.Range(start: 0, rand.Next(maxValue: 3) + 1)
+                                        .Select(_ => rand.Next())
+                                        .ToArray()
+                                )
+                                .ToArray()
+                        )
+                        .ToArray()
+                )
+                .ToArray()
+        };
+
+        var clone = original.DeepClone();
+
+        clone.Should()
+            .BeExactClone(original);
+    }
+
+    [GenerateDeepClone]
+    private partial class NestedArrayClass
+    {
+        public required int[][] NestedArray { get; init; }
+        public required int[][][][] DeeplyNestedArray { get; init; }
+    }
+}
