b77564240b64282ee39e57d3d29e7a5f40901414
Sander van 't Einde <github@sandervanteinde.nl>
2023-12-28 22:12:29 +0100

    Support multi dimensional arrays +semver:minor (#9)

---
+++ DeepCloneGenerator.Tests/DimensionalArraysTests.cs
@@ -0,0 +1,38 @@
+﻿namespace DeepCloneGenerator.Tests;
+
+public partial class DimensionalArraysTests
+{
+    [Fact]
+    public void Test()
+    {
+        var original = new ClassWithDimensionalArrays
+        {
+            TwoDimensionalArray = new[,]
+            {
+                { 1, 2, 3 },
+                { 4, 5, 6 }
+            },
+            ThreeDimensionalArray = new[,,]
+            {
+                {
+                    { 7, 8, 9 },
+                    { 10, 11, 12 },
+                    { 13, 14, 15 },
+                    { 16, 17, 18 }
+                }
+            }
+        };
+
+        var clone = original.DeepClone();
+
+        clone.Should()
+            .BeExactClone(original);
+    }
+
+    [GenerateDeepClone]
+    private partial class ClassWithDimensionalArrays
+    {
+        public required int[,] TwoDimensionalArray { get; init; }
+        public required int[,,] ThreeDimensionalArray { get; init; }
+    }
+}
