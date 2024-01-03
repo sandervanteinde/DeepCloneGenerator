718d483a8cd86289104b4459a23f3f5f7b442464
Sander van 't Einde <github@sandervanteinde.nl>
2024-01-03 21:58:32 +0100

    Code fixer init

---
+++ DeepCloneGenerator.Analyzers/PropertyIsReferenceCodeFixProvider.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace DeepCloneGenerator.Analyzers;
+
+[ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(PropertyIsReferenceCodeFixProvider))]
+[Shared]
+public class PropertyIsReferenceCodeFixProvider : CodeFixProvider
+{
+    public override ImmutableArray<string> FixableDiagnosticIds { get; } = ImmutableArray.Create(PropertyIsReferenceWithoutCloneAnalyzer.DiagnosticId);
+    
+    public override async Task RegisterCodeFixesAsync(CodeFixContext context)
+    {
+        var diagnostic = context.Diagnostics.Single();
+
+        var diagnosticSpan = diagnostic.Location.SourceSpan;
+        var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken)
+            .ConfigureAwait(false);
+
+        var diagnosticNode = root?.FindNode(diagnosticSpan);
+        var type = diagnosticNode switch
+        {
+            PropertyDeclarationSyntax property => property.Type,
+            _ => null
+        };
+
+        if (type is null)
+        {
+            return;
+        }
+        
+        context.RegisterCodeFix(
+            CodeAction.Create(
+                title: "Add [GenerateDeepClone] attribute",
+                createChangedDocument: c => AddAttribute(context.Document, type, c)
+            ),
+            diagnostic
+        );
+
+    }
+
+    private Task<Document> AddAttribute(Document document, TypeSyntax type, CancellationToken cancellationToken)
+    {
+        foreach (var doc in document.Project.Solution.Projects.SelectMany(proj => proj.Documents))
+        {
+            
+        }
+        return Task.FromResult(document);
+    }
+}
